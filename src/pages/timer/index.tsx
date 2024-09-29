61d2f71ad507f41686c0d372064e951bac681ac5
Marina Nagai <aniram_nba@yahoo.co.jp>
2024-09-29 20:00:55 +0900

    25分5分が繰り返されるように修正

--- src/pages/timer/index.tsx
+++ src/pages/timer/index.tsx
@@ -5,25 +5,36 @@ import TimerSettingForm from "@/components/TimerSettingForm";
 const Home: React.FC = () => {
   const router = useRouter();
   const [timerStarted, setTimerStarted] = useState(false);
+  const [currentTimer, setCurrentTimer] = useState("work");
   const [time, setTime] = useState(25 * 60); // 25分
-  const [nextTime, setNextTime] = useState(5 * 60); // 5分
 
   const startTimer = () => {
     setTimerStarted(true);
     router.push("/timer/measuring");
   };
 
+  const handleTimerEnd = () => {
+    if (currentTimer === "work") {
+      setCurrentTimer("break");
+      setTime(5 * 60); // 5分
+    } else {
+      setCurrentTimer("work");
+      setTime(25 * 60); // 25分
+    }
+  };
+
   return (
     <div>
       {timerStarted ? (
         <div>
-          <h1>計測中</h1>
-          <TimerSettingForm time={time} nextTime={nextTime} />
+          <h1>Recording</h1>
+          <TimerSettingForm time={time} />
+          <p>next {currentTimer === "work" ? "Break 5:00" : "Doing 25:00"}</p>
           <button onClick={() => setTimerStarted(false)}>停止</button>
         </div>
       ) : (
         <div>
-          <TimerSettingForm time={time} onChange={(time) => setTime(time)} />
+          <TimerSettingForm time={time} />
           <button onClick={startTimer}>スタート</button>
         </div>
       )}
