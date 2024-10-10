eb6d9659ab187645bb31dfd74418cc8a3eb6e1a3
Reina Koike <reina.koike.1212@gmail.com>
2024-10-10 21:48:24 +0900

    supabase.tsxの記述変更

--- src/pages/records/index.tsx
+++ src/pages/records/index.tsx
@@ -3,18 +3,20 @@ import React, { useEffect, useState } from "react";
 import { ThemeProvider } from "react-bootstrap";
 
 // supabaseを挿入
-const [Timer, setTimer] = useState<any>([]);
-
-useEffect(() => {
-  const getTimers = async () => {
-    const Timer = await getAllTimers();
-    setTimer(Timer);
-    console.log(Timer);
-  };
-  getTimers();
-}, []);
-
 const Home: React.FC = () => {
+  // supabaseからタイマーを取得するためのstate
+  const [Timer, setTimer] = useState<any>([]);
+
+  useEffect(() => {
+    const getTimers = async () => {
+      const Timer = await getAllTimers();
+      setTimer(Timer);
+      console.log(Timer);
+    };
+
+    getTimers();
+  }, []); // コンポーネントがマウントされたときに実行
+
   return <div>Records</div>;
 };
 
