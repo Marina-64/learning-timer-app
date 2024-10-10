1e83a6989dec3ddc4c4e620ad3f2e5ddc7bd4dc2
Reina Koike <reina.koike.1212@gmail.com>
2024-10-10 20:40:52 +0900

    client.tsx,servrr.tsxを削除し。supabase.tsに統合。

--- src/pages/records/index.tsx
+++ src/pages/records/index.tsx
@@ -1,6 +1,18 @@
-import React from "react";
+import { getAllTimers } from "@/utils/supabaseFunction";
+import React, { useEffect, useState } from "react";
+import { ThemeProvider } from "react-bootstrap";
 
 // supabaseを挿入
+const [Timer, setTimer] = useState<any>([]);
+
+useEffect(() => {
+  const getTimers = async () => {
+    const Timer = await getAllTimers();
+    setTimer(Timer);
+    console.log(Timer);
+  };
+  getTimers();
+}, []);
 
 const Home: React.FC = () => {
   return <div>Records</div>;
